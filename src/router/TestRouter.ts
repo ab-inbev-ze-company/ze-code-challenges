26c1f8e1eaef4eaa2907a7316189c00d5e4570b7
Laura <laura.nunes@labenu.com.br>
2022-09-27 16:51:02 -0300

    fix: caps router

--- src/router/TestRouter.ts
+++ src/router/TestRouter.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { TestBusiness } from "../business/testBusiness";
 import { TestController } from "../controller/testController";
 
+/*eslint new-cap: ["error", { "capIsNew": false }]*/
 export const testRouter = Router();
 export const testController = new TestController(
     new TestBusiness()
